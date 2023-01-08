920b31e9eabeeab8ac85080d2a042b6f3afe4986
Chynenna <121731763+Chynenna@users.noreply.github.com>
2023-01-08 13:57:51 +0200

    Showing current time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
@@ -10,6 +25,8 @@ function displayTemperature(response) {
     humidity.innerHTML = response.data.main.humidity;
     let wind = document.querySelector("#wind");
     wind.innerHTML = Math.round(response.data.wind.speed);
+    let time = document.querySelector("#time");
+    time.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "0e1ea42ab98d62ef5c3bd1f9fae41cdd";
